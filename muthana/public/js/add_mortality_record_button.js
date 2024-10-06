f8e82f04167f6f24faf5fb0e3c074e4c5bf33367
sli <ali@server1.soso.quest>
2024-10-06 07:09:24 +0000

    Mortality Record button finished

--- muthana/public/js/add_mortality_record_button.js
+++ muthana/public/js/add_mortality_record_button.js
@@ -1,13 +1,15 @@
 frappe.ui.form.on('Inpatient Record', {
   refresh: function(frm) {
-    frm.add_custom_button(__('Make Mortality Record'), function() {
-        // Redirect to new Mortality Record and map fields
-        frappe.new_doc('Mortality Record', {
-            'deceased_patient': frm.doc.patient,
-            'service_unit': frm.doc.addmission_service_unit_type
-        }).then(function(doc) {
-            frappe.set_route('Form', 'Mortality Record', doc.name);
-        });
-    }, __('Create'));
+      if (!frm.doc.__islocal) {
+          frm.add_custom_button(__('Make Mortality Record'), function() {
+              // Redirect to new Mortality Record and map fields
+              frappe.new_doc('Mortality Record', {
+                  'deceased_patient': frm.doc.patient,
+                  'service_unit': frm.doc.admission_service_unit_type
+              }).then(function(doc) {
+                  frappe.set_route('Form', 'Mortality Record', doc.name);
+              });
+          }, __('Create'));
+      }
   }
 });
