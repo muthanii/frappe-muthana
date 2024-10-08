139942597d09569b543145471856617aba775583
muthanii <muthanalsaadi@gmail.com>
2024-10-08 09:00:49 +0000

    Updates to the Inpatient Items DocType

--- muthana/muthana/doctype/inpatient_items/inpatient_items.py
+++ muthana/muthana/doctype/inpatient_items/inpatient_items.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2024, muthanii and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class InpatientItems(Document):
-	pass
+  pass
+	# def on_update(self):
+		# Get access to the Sales Invoice DocType:
+		# frappe.get_doc("Sales Invoice Item", "")
+
+		# Access to the main Child Table
+		# doc.item self.item_name
+		# self.quantity
+		# self.warehouse
+		# self.is_billed pass
