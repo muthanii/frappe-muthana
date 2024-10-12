0378dfc5955bb749d4790c764e409e276c4649ce
muthanii <muthanalsaadi@gmail.com>
2024-10-12 10:18:16 +0000

    Added a duplication checker function for sales invoice entries

--- muthana/muthana/api.py
+++ muthana/muthana/api.py
@@ -1,6 +1,10 @@
 import frappe
-# from frappe.utils import today
+from frappe.utils import today
 
+
+"""
+This is an API method to check for duplicate service requests in the Sales Invoice DocType.
+"""
 @frappe.whitelist()
 def count_duplicate_service_requests(patient, service_unit, docstatus):
     # posting_date = today()
@@ -16,3 +20,29 @@ def count_duplicate_service_requests(patient, service_unit, docstatus):
     )
 
     return duplicate_count
+
+
+"""
+This is an API method to get today's date for general usage.
+"""
+@frappe.whitelist()
+def get_today():
+    return today()
+
+
+"""
+This is a API method to check for duplicates in the Sales Invoice entries.
+"""
+@frappe.whitelist()
+def check_duplicate_sales_invoice_entries(patient):
+    today_date = today()
+
+    duplicates = frappe.db.count("Sales Invoice",
+                                 filters={
+                                     "patient": patient,
+                                     "creation_date": today_date,
+                                     "docstatus": 0
+                                 })
+    if duplicates > 1:
+        return "Duplicates found!"
+
